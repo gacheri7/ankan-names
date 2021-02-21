86808bcc0047e61718a4370d79f244aed57ed809
gacheri7 <martharollier00@gmail.com>
2021-02-22 02:55:26 +0300

    ass styles.css changes to container,body,h1,h2

---
+++ bootstrap-4.6.0-dist/js/script.js
@@ -0,0 +1,33 @@
+
+function calculate() {
+  //select element by id
+  const month = document.getElementById("month").value;
+  const day = document.getElementById("day").value;
+  var year = document.getElementById("year").value;
+  var gender = document.getElementById("gender").value;
+  var dob = month + " " + day + ", " + year;
+  var then = new Date(dob);
+  var theday = then.getDay() + 1;
+  var date = then.getDate();
+  year = then.getYear();
+  if (year < 100) year = "19" + then.getYear();
+  else year = then.getYear();
+//if gender is female ,give the following names
+  if (gender == "female") {
+      var weekday = new Array(6);
+      weekday[1] = "Akosua";
+      weekday[2] = "Adwoa";
+      weekday[3] = "Abenaa";
+      weekday[4] = "Akua";
+      weekday[5] = "Yaa";
+      weekday[6] = "Afua";
+      weekday[7] = "Ama";
+  } else if (gender == "male") {
+      var weekday = new Array(6);
+      weekday[1] = "Kwasi";
+      weekday[2] = "Kwadwo";
+      weekday[3] = "Kwabena";
+      weekday[4] = "Kwaku";
+      weekday[5] = "Yaw";
+      weekday[6] = "Kofi";
+      weekday[7] = "Kwame";
